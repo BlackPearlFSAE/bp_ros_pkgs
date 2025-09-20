21065fdadd66b93393b3087eeae868c8e8fa4419
To-en <toenen1243@gmail.com>
2025-09-20 23:09:14 +0700

    Success-network-device3

--- can_aggregator/src/temp.cpp
+++ can_aggregator/src/temp.cpp
@@ -34,23 +34,29 @@ class TimestamperWrapper : public rclcpp::Node {
 // Inherit rclcpp::Node create a publisher node 
 private:
   //  Topic list
-  std::string canRxtopic = "/CAN/vcan0/receive";
-  // std::string canRxtopic = "receive";
+  std::string canTx = "/CAN/can0/transmit";
+  std::string canRx = "/CAN/can0/receive";
+
+  // in case of reverting back to microROS serial
+  std::string TopicAMS = "AMS";
+  std::string TopicBAMO = "BAMO";
+  std::string TopicFront = "Front";
+  std::string TopicRear = "Rear";
+
+  enum TopicType { AMS, BAMO, FRONT, REAR };
+
   enum MessageID { AMS = 0x00 , BAMO = 0x01, FRONT = 0x02, REAR = 0x03 };
   // otherwise than this, we will set a condition based on bp16 communication agreement that we needs
+
   rclcpp::TimerBase::SharedPtr timer_task1;
   rclcpp::TimerBase::SharedPtr timer_task2;
   size_t count_;
   
-  // Create publisher Node pointer of type CAN Frame
-  
-  rclcpp::Subscription<Frame>::SharedPtr sub;
+  // Create publisher Node pointer of type CAN Frame,  
   rclcpp::Publisher<Frame>::SharedPtr pub_ams;
   rclcpp::Publisher<Frame>::SharedPtr pub_bamo;
   rclcpp::Publisher<Frame>::SharedPtr pub_front;
   rclcpp::Publisher<Frame>::SharedPtr pub_rear;
-
-  // rclcpp::Publisher<Frame>::SharedPtr bmu;
 
   // Define can frame
   Frame last_ams_msg;
@@ -58,62 +64,67 @@ private:
   Frame last_front_msg;
   Frame last_rear_msg;
 
-  // Later this should be std vector of pointer to frame or just frame
-    // might optimize later
-  // Frame bmu1;
-  // Frame bmu2;
-  // Frame bmu3;
-  // Frame bmu4;
-  // Frame bmu5;
-  // Frame bmu6;
-  // Frame bmu7;
-  // Frame bmu8;
-
   // There should be only 4 message , but the worrysome case is BMU internal bus that I supppose to be a separate communication
   // channel, since it isn't
 
 public:
   // class variable here
   TimestamperWrapper(): Node("TimestamperWrapper"), count_(0)
-  {    
+  {
+    auto sub_cb = [this](const Frame::SharedPtr msg, TopicType type) {
+      // int64_t now = duration_cast<nanoseconds>(system_clock::now().time_since_epoch()).count();
+      int64_t now = this->get_clock()->now().nanoseconds();
+      switch (type) {
+        // assign our own CAN frame to recivied msg 
+        case AMS: last_ams_msg = *msg; last_ams_msg.header.stamp.nanosec = now;       break;
+        case BAMO: last_bamo_msg = *msg; last_bamo_msg.header.stamp.nanosec = now;    break;
+        case FRONT: last_front_msg = *msg; last_front_msg.header.stamp.nanosec = now; break;
+        case REAR: last_rear_msg = *msg; last_rear_msg.header.stamp.nanosec = now;    break;
+      }
+    };
+
+    
     // The subscribed message comes from ROS2socketcan, provide template messageT name
     // call sub_cb lambda expression above when subscribed topics
-    sub = this->create_subscription<Frame>(canRxtopic, 10,
-      std::bind(&TimestamperWrapper::sub_sbRx, this, std::placeholders::_1));
+    this->create_subscription<Frame>(TopicAMS, 10,
+      [sub_cb](const Frame::SharedPtr msg) { sub_cb(msg, AMS);});
+    this->create_subscription<Frame>(TopicBAMO, 10, 
+      [sub_cb](const Frame::SharedPtr msg) { sub_cb(msg, BAMO);});
+    this->create_subscription<Frame>(TopicFront, 10,
+      [sub_cb](const Frame::SharedPtr msg) { sub_cb(msg, FRONT);});
+    this->create_subscription<Frame>(TopicRear, 10,
+      [sub_cb](const Frame::SharedPtr msg) { sub_cb(msg, REAR);});
+      // std::bind(&TimestamperWrapper::callback_, this, std::placeholders::_4));
 
-    pub_ams = this->create_publisher<Frame>("AMS_stamped",10);
-    pub_bamo = this->create_publisher<Frame>("BAMO_stamped",10);
-    pub_front = this->create_publisher<Frame>("Front_stamped",10);
-    pub_rear = this->create_publisher<Frame>("Rear_stamped",10);
-    
-    // bmu = this->create_publisher<Frame>("BMU",10);
+    pub_ams = this->create_publisher<Frame>(TopicAMS+"_stamped",10);
+    pub_bamo = this->create_publisher<Frame>(TopicBAMO+"_stamped",10);
+    pub_front = this->create_publisher<Frame>(TopicFront+"_stamped",10);
+    pub_rear = this->create_publisher<Frame>(TopicRear+"_stamped",10);
 
-    // Timer for publishing callback
-    // Timer for debugger callback
+    // why do I need to bind the callback method to the currently refering object
     timer_task1 = this->create_wall_timer(TIMER_PUBLISH_PERIOD_MS, std::bind(&TimestamperWrapper::publish_topic, this));
     timer_task2 = this->create_wall_timer(TIMER_PUBLISH_PERIOD_MS, std::bind(&TimestamperWrapper::debugger,this));
   };
 
-  void sub_sbRx(const Frame::SharedPtr msg){
-    // RCLCPP_INFO(this->get_logger(),"I know you got ");
+  void sub_sbTx(const Frame::SharedPtr msg, uint16_t standardID , uint8_t dlc, uint8_t* Data){
     int64_t now = this->get_clock()->now().nanoseconds();
-    // welp still not sure how this would work
-    MessageID ID = static_cast<MessageID>(msg->id);
-    
+    msg->header.stamp.nanosec = now;
+    msg->dlc = dlc;
+    // Copy data from Data pointer to the first index of msg->data
+    std::copy(Data, Data + dlc, msg->data.begin());
+    // for(int i = 0; i< dlc; i++)
+    //   msg->data[i] = Data[i];
+  }
+
+  void sub_sbRx(const Frame::SharedPtr msg, MessageID ID){
+    int64_t now = this->get_clock()->now().nanoseconds();
+
     switch (ID) {
       // assign our own CAN frame to each recivied ID 
-      case AMS: 
-        last_ams_msg = *msg; last_ams_msg.header.stamp.nanosec = now;     break;
-      case BAMO: 
-        last_bamo_msg = *msg; last_bamo_msg.header.stamp.nanosec = now;   break;
-      case FRONT: 
-        last_front_msg = *msg; last_front_msg.header.stamp.nanosec = now; break;
-      case REAR: 
-        last_rear_msg = *msg; last_rear_msg.header.stamp.nanosec = now;   break;
-      
-      // if switch case doesn't comply with enum -> it must be BMU internal bus
-      default:
-        ;
+      case AMS: last_ams_msg = *msg; last_ams_msg.header.stamp.nanosec = now;       break;
+      case BAMO: last_bamo_msg = *msg; last_bamo_msg.header.stamp.nanosec = now;    break;
+      case FRONT: last_front_msg = *msg; last_front_msg.header.stamp.nanosec = now; break;
+      case REAR: last_rear_msg = *msg; last_rear_msg.header.stamp.nanosec = now;    break;
     }
   }
 
@@ -125,10 +136,14 @@ public:
   }
 
   void debugger(){
-    RCLCPP_INFO(this->get_logger(),"1.AMS CAN Frame: %03x", last_ams_msg.id);
-    RCLCPP_INFO(this->get_logger(),"2.BAMO CAN Frame: %03x", last_bamo_msg.id);
-    RCLCPP_INFO(this->get_logger(),"3.FRONT Frame: %03x", last_front_msg.id);
-    RCLCPP_INFO(this->get_logger(),"4.REAR Frame: %03x", last_rear_msg.id);
+    // int64_t now = this->get_clock()->now().nanoseconds();
+    // RCLCPP_INFO(this->get_logger(),"0.TIME: %ld", now);
+    /*Time function is valid*/
+
+    RCLCPP_INFO(this->get_logger(),"1.AMS CAN Frame: %0x", last_ams_msg.id);
+    RCLCPP_INFO(this->get_logger(),"2.BAMO CAN Frame: %0x", last_bamo_msg.id);
+    RCLCPP_INFO(this->get_logger(),"3.FRONT Frame: %0x", last_front_msg.id);
+    RCLCPP_INFO(this->get_logger(),"4.REAR Frame: %0x", last_rear_msg.id);
     // Debug the messsage inside (won't translate)
   }
 };
